2249af92b0795b6fde0960edfdfa64ed938c18c0
Amey Vaijapurkar <ameyva722@gmail.com>
2025-03-25 09:43:00 +0800

    Delete tsv2bed.sh

--- tsv2bed.sh
+++
@@ -1,9 +0,0 @@
-awk -F '\t' '{
-  split($1, coords, "[:-]");
-  chr = coords[1];
-  start = coords[2];
-  end = coords[3];
-  strand = coords[4];
-  score = $2;
-  print chr "\t" start "\t" end "\t" score "\t" "." "\t" strand;
-}' gtex_junctions_with_psr.tsv > gtex_junctions_with_psr.bed
